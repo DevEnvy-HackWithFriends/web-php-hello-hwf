ecd1d49ef215857377870eee21706aa24f62fcb3
mdemerat <mdemerat@med.umich.edu>
2017-03-03 15:36:00 -0500

    added project name

---
+++ cheStatus.sh
@@ -0,0 +1,1 @@
+docker run -it --rm --name CHE_HWF -v /var/run/docker.sock:/var/run/docker.sock -v $PWD/data:/data -v $PWD/:/chedir eclipse/che:latest dir status 
--- index.php
+++ index.php
@@ -1,7 +1,12 @@
+<?php
+echo "PHP Web DevEnvy Project"
+?>
+</br>
 <img src="devenvy_logo_500.png">
 
 <br/>
 <?php
 echo "Welcome to Hack With Friends 2017!  DevEnvy Project"
+
 
 ?>
